e3a4cb050fd1a1df175089bd900d08c24cce76db
Lenn <lennperik@hotmail.nl>
2024-08-27 21:31:04 +0000

    Add CONFIG_SCHEMA

--- __init__.py
+++ __init__.py
@@ -2,12 +2,35 @@
 
 from __future__ import annotations
 
-from homeassistant.const import Platform
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_ID, CONF_NAME, CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
+from .const import CONF_CONTROL_UNITS, CONF_LOGIN, CONF_SCENES, DOMAIN
+
+CONTROL_UNIT_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_ID): cv.positive_int,
+})
+
+SCENE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_ID): cv.positive_int,
+})
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_PORT): cv.positive_int,
+        vol.Optional(CONF_LOGIN): cv.string,
+        vol.Required(CONF_CONTROL_UNITS): vol.All(cv.ensure_list, [CONTROL_UNIT_SCHEMA]),
+        vol.Required(CONF_SCENES): vol.All(cv.ensure_list, [SCENE_SCHEMA]),
+    })
+}, extra=vol.ALLOW_EXTRA)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
--- const.py
+++ const.py
@@ -4,6 +4,10 @@ from __future__ import annotations
 
 from datetime import timedelta
 
+CONF_LOGIN = "login"
+CONF_CONTROL_UNITS = "control_units"
+CONF_SCENES = "scenes"
+
 DOMAIN = "grafik_eye"
 DISPLAY_NAME = "Grafik Eye 3000: {control_unit_name}"
 
